a8994a68d82b894167d125dae96483605431ab78
tuttieee <t.yic.yt@gmail.com>
2019-01-07 23:27:50 +0900

    Refactoring test

--- src/test/emulator.test.ts
+++ src/test/emulator.test.ts
@@ -31,7 +31,7 @@ async function clearTextEditor(textEditor: vscode.TextEditor) {
 async function cleanUpWorkspace() {
 }
 
-suite("killRegion plus yank", () => {
+suite("Emulator with text editing", () => {
     let activeTextEditor: vscode.TextEditor;
 
     setup(async () => {
@@ -43,105 +43,97 @@ ABCDEFGHIJ`;
 
     teardown(cleanUpWorkspace);
 
-    test("it sorts ranges and aggregates the selected texts in order when multi cursor mode", async () => {
-        const emulator = new EmacsEmulator(activeTextEditor);
+    suite("killRegion with yank", () => {
+        test("it sorts ranges and aggregates the selected texts in order when multi cursor mode", async () => {
+            const emulator = new EmacsEmulator(activeTextEditor);
 
-        // Select with multi cursor in not aligned order
-        activeTextEditor.selections = [
-            new Selection(new Position(1, 0), new Position(1, 3)),
-            new Selection(new Position(0, 0), new Position(0, 3)),
-            new Selection(new Position(2, 0), new Position(2, 3)),
-        ];
-        await emulator.killRegion();
+            // Select with multi cursor in not aligned order
+            activeTextEditor.selections = [
+                new Selection(new Position(1, 0), new Position(1, 3)),
+                new Selection(new Position(0, 0), new Position(0, 3)),
+                new Selection(new Position(2, 0), new Position(2, 3)),
+            ];
+            await emulator.killRegion();
 
-        assert.equal(
-            activeTextEditor.document.getText(),
-            `3456789
+            assert.equal(
+                activeTextEditor.document.getText(),
+                `3456789
 defghij
 DEFGHIJ`,
-        );
+            );
 
-        clearTextEditor(activeTextEditor);
+            clearTextEditor(activeTextEditor);
 
-        activeTextEditor.selections = [
-            new Selection(new Position(0, 0), new Position(0, 0)),
-        ];
-        await emulator.yank();
+            activeTextEditor.selections = [
+                new Selection(new Position(0, 0), new Position(0, 0)),
+            ];
+            await emulator.yank();
 
-        assert.equal(
-            activeTextEditor.document.getText(),
-            `012
+            assert.equal(
+                activeTextEditor.document.getText(),
+                `012
 abc
 ABC`,
-        );
-    });
-});
+            );
+        });
 
-suite("killLine plus yank", () => {
-    let activeTextEditor: vscode.TextEditor;
-
-    setup(async () => {
-        const initialText = `0123456789
-abcdefghij
-ABCDEFGHIJ`;
-        activeTextEditor = await setupWorkspace(initialText);
     });
 
-    teardown(cleanUpWorkspace);
+    suite("killLine with yank", () => {
+        test("it cuts the current line", async () => {
+            const emulator = new EmacsEmulator(activeTextEditor);
 
-    test("it cuts the current line", async () => {
-        const emulator = new EmacsEmulator(activeTextEditor);
+            activeTextEditor.selections = [
+                new Selection(new Position(1, 1), new Position(1, 1)),  // Line_1
+            ];
 
-        activeTextEditor.selections = [
-            new Selection(new Position(1, 1), new Position(1, 1)),  // Line_1
-        ];
+            await emulator.killLine();
 
-        await emulator.killLine();
-
-        assert.equal(
-            activeTextEditor.document.getText(),
-            `0123456789
+            assert.equal(
+                activeTextEditor.document.getText(),
+                `0123456789
 a
 ABCDEFGHIJ`,
-        );
+            );
 
-        clearTextEditor(activeTextEditor);
+            clearTextEditor(activeTextEditor);
 
-        activeTextEditor.selections = [
-            new Selection(new Position(0, 0), new Position(0, 0)),
-        ];
-        await emulator.yank();
+            activeTextEditor.selections = [
+                new Selection(new Position(0, 0), new Position(0, 0)),
+            ];
+            await emulator.yank();
 
-        assert.equal(
-            activeTextEditor.document.getText(),
-            "bcdefghij",
-        );
-    });
+            assert.equal(
+                activeTextEditor.document.getText(),
+                "bcdefghij",
+            );
+        });
 
-    test("it removes line break if invoked at the end of line", async () => {
-        const emulator = new EmacsEmulator(activeTextEditor);
+        test("it removes line break if invoked at the end of line", async () => {
+            const emulator = new EmacsEmulator(activeTextEditor);
 
-        activeTextEditor.selections = [
-            new Selection(new Position(1, 10), new Position(1, 10)),  // at the end of line_1
-        ];
-        await emulator.killLine();
+            activeTextEditor.selections = [
+                new Selection(new Position(1, 10), new Position(1, 10)),  // at the end of line_1
+            ];
+            await emulator.killLine();
 
-        assert.equal(
-            activeTextEditor.document.getText(),
-            `0123456789
+            assert.equal(
+                activeTextEditor.document.getText(),
+                `0123456789
 abcdefghijABCDEFGHIJ`,
-        );
+            );
 
-        clearTextEditor(activeTextEditor);
+            clearTextEditor(activeTextEditor);
 
-        activeTextEditor.selections = [
-            new Selection(new Position(0, 0), new Position(0, 0)),
-        ];
-        await emulator.yank();
+            activeTextEditor.selections = [
+                new Selection(new Position(0, 0), new Position(0, 0)),
+            ];
+            await emulator.yank();
 
-        assert.equal(
-            activeTextEditor.document.getText(),
-            "\n",
-        );
+            assert.equal(
+                activeTextEditor.document.getText(),
+                "\n",
+            );
+        });
     });
 });
