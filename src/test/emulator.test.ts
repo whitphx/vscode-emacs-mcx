ebd84bf912e8c5415e794b5c6a258b38e6b5b208
tuttieee <t.yic.yt@gmail.com>
2019-01-07 23:14:50 +0900

    Refactoring test

--- src/test/emulator.test.ts
+++ src/test/emulator.test.ts
@@ -1,25 +1,49 @@
 import * as assert from "assert";
 import * as vscode from "vscode";
-import {Position, Selection} from "vscode";
-import {EmacsEmulator} from "../emulator";
+import {Position, Range, Selection} from "vscode";
+import { EmacsEmulator } from "../emulator";
 
-suite("EmacsEmulator.killRegion() and .yank()", () => {
-    test("it sorts ranges and aggregates the selected texts in order when multi cursor mode", async () => {
-        const content = `0123456789
+async function setupWorkspace(initialText: string = ""): Promise<vscode.TextEditor> {
+    const doc = await vscode.workspace.openTextDocument({
+        content: initialText,
+        language: "text",
+    });
+
+    await vscode.window.showTextDocument(doc);
+
+    const activeTextEditor = vscode.window.activeTextEditor;
+    assert.ok(activeTextEditor);
+
+    return activeTextEditor!;
+}
+
+async function clearTextEditor(textEditor: vscode.TextEditor) {
+    const doc = textEditor.document;
+    await textEditor.edit((editBuilder) => {
+        editBuilder.delete(new Range(
+            new Position(0, 0),
+            doc.positionAt(doc.getText().length),
+        ));
+    });
+    assert.equal(doc.getText(), "");
+}
+
+async function cleanUpWorkspace() {
+}
+
+suite("killRegion plus yank", () => {
+    let activeTextEditor: vscode.TextEditor;
+
+    setup(async () => {
+        const initialText = `0123456789
 abcdefghij
 ABCDEFGHIJ`;
+        activeTextEditor = await setupWorkspace(initialText);
+    });
 
-        const doc = await vscode.workspace.openTextDocument({
-            content,
-            language: "text",
-        });
-        await vscode.window.showTextDocument(doc);
+    teardown(cleanUpWorkspace);
 
-        const activeTextEditor = vscode.window.activeTextEditor;
-        if (typeof activeTextEditor === "undefined") {
-            throw Error("vscode.window.activeTextEditor");
-        }
-
+    test("it sorts ranges and aggregates the selected texts in order when multi cursor mode", async () => {
         const emulator = new EmacsEmulator(activeTextEditor);
 
         // Select with multi cursor in not aligned order
@@ -28,30 +52,24 @@ ABCDEFGHIJ`;
             new Selection(new Position(0, 0), new Position(0, 3)),
             new Selection(new Position(2, 0), new Position(2, 3)),
         ];
-
         await emulator.killRegion();
 
         assert.equal(
-            doc.getText(),
+            activeTextEditor.document.getText(),
             `3456789
 defghij
 DEFGHIJ`,
         );
 
-        // Open a empty document
-        const yankDoc = await vscode.workspace.openTextDocument({
-            content: "",
-            language: "text",
-        });
-        await vscode.window.showTextDocument(yankDoc);
+        clearTextEditor(activeTextEditor);
+
         activeTextEditor.selections = [
             new Selection(new Position(0, 0), new Position(0, 0)),
         ];
-
         await emulator.yank();
 
         assert.equal(
-            yankDoc.getText(),
+            activeTextEditor.document.getText(),
             `012
 abc
 ABC`,
@@ -59,26 +77,21 @@ ABC`,
     });
 });
 
-suite("EmacsEmulator.killLine() and .yank()", () => {
-    test("it cuts the current line", async () => {
-        const content = `0123456789
+suite("killLine plus yank", () => {
+    let activeTextEditor: vscode.TextEditor;
+
+    setup(async () => {
+        const initialText = `0123456789
 abcdefghij
 ABCDEFGHIJ`;
+        activeTextEditor = await setupWorkspace(initialText);
+    });
 
-        const doc = await vscode.workspace.openTextDocument({
-            content,
-            language: "text",
-        });
-        await vscode.window.showTextDocument(doc);
+    teardown(cleanUpWorkspace);
 
-        const activeTextEditor = vscode.window.activeTextEditor;
-        if (typeof activeTextEditor === "undefined") {
-            throw Error("vscode.window.activeTextEditor");
-        }
-
+    test("it cuts the current line", async () => {
         const emulator = new EmacsEmulator(activeTextEditor);
 
-        // Select with single cursor without selection
         activeTextEditor.selections = [
             new Selection(new Position(1, 1), new Position(1, 1)),  // Line_1
         ];
@@ -86,77 +99,49 @@ ABCDEFGHIJ`;
         await emulator.killLine();
 
         assert.equal(
-            doc.getText(),
+            activeTextEditor.document.getText(),
             `0123456789
 a
 ABCDEFGHIJ`,
         );
 
-        // Open a empty document
-        const yankDoc = await vscode.workspace.openTextDocument({
-            content: "",
-            language: "text",
-        });
-        await vscode.window.showTextDocument(yankDoc);
+        clearTextEditor(activeTextEditor);
+
         activeTextEditor.selections = [
             new Selection(new Position(0, 0), new Position(0, 0)),
         ];
-
         await emulator.yank();
 
         assert.equal(
-            yankDoc.getText(),
-            `bcdefghij`,
+            activeTextEditor.document.getText(),
+            "bcdefghij",
         );
     });
 
-
     test("it removes line break if invoked at the end of line", async () => {
-        const content = `0123456789
-abcdefghij
-ABCDEFGHIJ`;
-
-        const doc = await vscode.workspace.openTextDocument({
-            content,
-            language: "text",
-        });
-        await vscode.window.showTextDocument(doc);
-
-        const activeTextEditor = vscode.window.activeTextEditor;
-        if (typeof activeTextEditor === "undefined") {
-            throw Error("vscode.window.activeTextEditor");
-        }
-
         const emulator = new EmacsEmulator(activeTextEditor);
 
-        // Select with single cursor without selection
         activeTextEditor.selections = [
             new Selection(new Position(1, 10), new Position(1, 10)),  // at the end of line_1
         ];
-
         await emulator.killLine();
 
         assert.equal(
-            doc.getText(),
+            activeTextEditor.document.getText(),
             `0123456789
 abcdefghijABCDEFGHIJ`,
         );
 
-        // Open a empty document
-        const yankDoc = await vscode.workspace.openTextDocument({
-            content: "",
-            language: "text",
-        });
-        await vscode.window.showTextDocument(yankDoc);
+        clearTextEditor(activeTextEditor);
+
         activeTextEditor.selections = [
             new Selection(new Position(0, 0), new Position(0, 0)),
         ];
-
         await emulator.yank();
 
         assert.equal(
-            yankDoc.getText(),
-            `\n`,
+            activeTextEditor.document.getText(),
+            "\n",
         );
     });
 });
