12b2ae4e4911ca029f902ebbadd51e7800e6e75b
Yuichiro Tsuchiya <t.yic.yt@gmail.com>
2020-03-16 23:00:23 +0900

    Set forbidOnly option to mocha

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -5,7 +5,8 @@ import * as glob from "glob";
 export function run(): Promise<void> {
   // Create the mocha test
   const mocha = new Mocha({
-    ui: "tdd"
+    ui: "tdd",
+    forbidOnly: true
   });
   mocha.useColors(true);
 
