2fa53dbf20e1f8731fd0e3b4e7294e1a886b39e2
Yuichi Nakmaura <yunk@ya2.so-net.ne.jp>
2020-03-19 08:08:10 +0900

    Revert "Change ctrl+l (recenter) behavior to original if strictEmacsMove is true."
    This reverts commit c844b8dbcb245d11690813a4fdfde30d4788805f.

--- src/commands/recenter.ts
+++ src/commands/recenter.ts
@@ -1,7 +1,6 @@
 import * as vscode from "vscode";
 import { TextEditor, TextEditorRevealType } from "vscode";
 import { EmacsCommand, IEmacsCommandInterrupted } from ".";
-import { Configuration } from "../configuration/configuration";
 
 enum RecenterPosition {
   Middle,
@@ -15,38 +14,34 @@ export class RecenterTopBottom extends EmacsCommand implements IEmacsCommandInte
   private recenterPosition: RecenterPosition = RecenterPosition.Middle;
 
   public execute(textEditor: TextEditor, isInMarkMode: boolean, prefixArgument: number | undefined) {
-    if (Configuration.instance.strictEmacsMove) {
-      textEditor.revealRange(textEditor.selection, TextEditorRevealType.InCenter);
-    } else {
-      switch (this.recenterPosition) {
-        case RecenterPosition.Middle: {
-          textEditor.revealRange(textEditor.selection, TextEditorRevealType.InCenter);
-          this.recenterPosition = RecenterPosition.Top;
-          break;
-        }
-        case RecenterPosition.Top: {
-          textEditor.revealRange(textEditor.selection, TextEditorRevealType.AtTop);
-          this.recenterPosition = RecenterPosition.Bottom;
-          break;
-        }
-        case RecenterPosition.Bottom: {
-          // TextEditor.revealRange does not supprt to set the cursor at the bottom of window.
-          // Therefore, the number of lines to scroll is calculated here.
-          const current = textEditor.selection.active.line;
-          const visibleTop = textEditor.visibleRanges[0].start.line;
-          const visibleBottom = textEditor.visibleRanges[0].end.line;
-          const visibleHeight = visibleBottom - visibleTop;
+    switch (this.recenterPosition) {
+      case RecenterPosition.Middle: {
+        textEditor.revealRange(textEditor.selection, TextEditorRevealType.InCenter);
+        this.recenterPosition = RecenterPosition.Top;
+        break;
+      }
+      case RecenterPosition.Top: {
+        textEditor.revealRange(textEditor.selection, TextEditorRevealType.AtTop);
+        this.recenterPosition = RecenterPosition.Bottom;
+        break;
+      }
+      case RecenterPosition.Bottom: {
+        // TextEditor.revealRange does not supprt to set the cursor at the bottom of window.
+        // Therefore, the number of lines to scroll is calculated here.
+        const current = textEditor.selection.active.line;
+        const visibleTop = textEditor.visibleRanges[0].start.line;
+        const visibleBottom = textEditor.visibleRanges[0].end.line;
+        const visibleHeight = visibleBottom - visibleTop;
 
-          const nextVisibleTop = Math.max(current - visibleHeight, 1);
+        const nextVisibleTop = Math.max(current - visibleHeight, 1);
 
-          // Scroll so that `nextVisibleTop` is the top of window
-          const p = new vscode.Position(nextVisibleTop, 0);
-          const r = new vscode.Range(p, p);
-          textEditor.revealRange(r);
+        // Scroll so that `nextVisibleTop` is the top of window
+        const p = new vscode.Position(nextVisibleTop, 0);
+        const r = new vscode.Range(p, p);
+        textEditor.revealRange(r);
 
-          this.recenterPosition = RecenterPosition.Middle;
-          break;
-        }
+        this.recenterPosition = RecenterPosition.Middle;
+        break;
       }
     }
   }
