7726a88dca64f4421c934043782e54824aa8aaa5
Yuichiro Tsuchiya <t.yic.yt@gmail.com>
2020-04-25 21:43:46 +0900

    Create keybinding-generator/cli.ts

---
+++ keybinding-generator/cli.ts
@@ -0,0 +1,25 @@
+import * as fs from "fs";
+import { KeyBinding, KeyBindingSource, generateKeybindings } from "./generate-keybindings";
+
+const srcFilePath = "./keybindings.json";
+const packageDotJsonPath = "./package.json";
+
+console.info(`Reading ${srcFilePath} ...`);
+const srcContent = fs.readFileSync(srcFilePath, "utf8");
+const srcJSON = JSON.parse(srcContent);
+const keybindingSrcs: KeyBindingSource[] = srcJSON["keybindings"];
+
+let dstKeybindings: KeyBinding[] = [];
+
+keybindingSrcs.forEach((keybindingSrc) => {
+  const keybindings = generateKeybindings(keybindingSrc);
+  dstKeybindings = dstKeybindings.concat(keybindings);
+});
+
+console.info(`Reading ${packageDotJsonPath} ...`);
+const packageJsonContent = fs.readFileSync(packageDotJsonPath, "utf8");
+const packageJson = JSON.parse(packageJsonContent);
+
+console.info(`Overwriting ${packageDotJsonPath} ...`);
+packageJson["contributes"]["keybindings"] = dstKeybindings;
+fs.writeFileSync(packageDotJsonPath, JSON.stringify(packageJson, null, "\t"));
