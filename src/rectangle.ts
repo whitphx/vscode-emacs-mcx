ee8b9505253d43391dc81ed939940ba5764a5be4
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2021-05-23 16:53:01 +0900

    Update convertSelectionToRectSelections with tests

--- src/rectangle.ts
+++ src/rectangle.ts
@@ -4,17 +4,23 @@ export function convertSelectionToRectSelections(
   document: vscode.TextDocument,
   selection: vscode.Selection
 ): vscode.Selection[] {
-  const lineDelta = selection.active.line > selection.anchor.line ? 1 : -1;
-
   const rectSelections: vscode.Selection[] = [];
-  for (let line = selection.anchor.line; line !== selection.active.line + lineDelta; line += lineDelta) {
+  const lineStart = Math.max(selection.start.line, 0);
+  const lineEnd = Math.min(selection.end.line, document.lineCount - 1);
+  for (let line = lineStart; line <= lineEnd; line++) {
     const maxChar = document.lineAt(line).range.end.character;
     rectSelections.push(
       new vscode.Selection(
-        new vscode.Position(line, Math.min(selection.anchor.character, maxChar)),
-        new vscode.Position(line, Math.min(selection.active.character, maxChar))
+        line,
+        Math.min(selection.anchor.character, maxChar),
+        line,
+        Math.min(selection.active.character, maxChar)
       )
     );
+  }
+
+  if (selection.active.line < selection.anchor.line) {
+    rectSelections.reverse();
   }
 
   return rectSelections;
