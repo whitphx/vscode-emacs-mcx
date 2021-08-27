fb782ee56615e3dcc3b252c8bd3f54d175f5c00f
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2021-08-27 23:02:53 +0900

    Add files for web extension and disable the winston-based logger

--- src/logger.ts
+++ src/logger.ts
@@ -2,64 +2,69 @@
  * This file is derived from VSCodeVim/Vim.
  */
 
-import * as vscode from "vscode";
-import * as winston from "winston";
-import { ConsoleForElectron } from "winston-console-for-electron";
-import TransportStream from "winston-transport";
-import { IConfiguration } from "./configuration/iconfiguration";
+// import * as vscode from "vscode";
+// import * as winston from "winston";
+// import { ConsoleForElectron } from "winston-console-for-electron";
+// import TransportStream from "winston-transport";
+// import { IConfiguration } from "./configuration/iconfiguration";
 
-/**
- * Implementation of Winston transport
- * Displays VS Code message to user
- */
-class VsCodeMessage extends TransportStream {
-  public log(info: { level: string; message: string }, callback: () => void) {
-    switch (info.level) {
-      case "error":
-        vscode.window.showErrorMessage(info.message, "Dismiss");
-        break;
-      case "warn":
-        vscode.window.showWarningMessage(info.message, "Dismiss");
-        break;
-      case "info":
-      case "verbose":
-      case "debug":
-        vscode.window.showInformationMessage(info.message, "Dismiss");
-        break;
-    }
+// /**
+//  * Implementation of Winston transport
+//  * Displays VS Code message to user
+//  */
+// class VsCodeMessage extends TransportStream {
+//   public log(info: { level: string; message: string }, callback: () => void) {
+//     switch (info.level) {
+//       case "error":
+//         vscode.window.showErrorMessage(info.message, "Dismiss");
+//         break;
+//       case "warn":
+//         vscode.window.showWarningMessage(info.message, "Dismiss");
+//         break;
+//       case "info":
+//       case "verbose":
+//       case "debug":
+//         vscode.window.showInformationMessage(info.message, "Dismiss");
+//         break;
+//     }
 
-    if (callback) {
-      callback();
-    }
-  }
+//     if (callback) {
+//       callback();
+//     }
+//   }
+// }
+
+export const logger = console;
+// export let logger = winston.createLogger({
+//   format: winston.format.simple(),
+//   transports: [
+//     new ConsoleForElectron({
+//       level: "error",
+//       silent: false,
+//     }),
+//     new VsCodeMessage({
+//       level: "error",
+//       silent: false,
+//     }),
+//   ],
+// });
+
+export function initializeLogger(config: any) {
+  console.log(config);
+  return null;
 }
-
-export let logger = winston.createLogger({
-  format: winston.format.simple(),
-  transports: [
-    new ConsoleForElectron({
-      level: "error",
-      silent: false,
-    }),
-    new VsCodeMessage({
-      level: "error",
-      silent: false,
-    }),
-  ],
-});
-
-export function initializeLogger(configuration: IConfiguration) {
-  logger = winston.createLogger({
-    format: winston.format.simple(),
-    transports: [
-      new ConsoleForElectron({
-        level: configuration.debug.loggingLevelForConsole,
-        silent: configuration.debug.silent,
-      }),
-      new VsCodeMessage({
-        level: configuration.debug.loggingLevelForAlert,
-        silent: configuration.debug.silent,
-      }),
-    ],
-  });
-}
+// export function initializeLogger(configuration: IConfiguration) {
+//   logger = winston.createLogger({
+//     format: winston.format.simple(),
+//     transports: [
+//       new ConsoleForElectron({
+//         level: configuration.debug.loggingLevelForConsole,
+//         silent: configuration.debug.silent,
+//       }),
+//       new VsCodeMessage({
+//         level: configuration.debug.loggingLevelForAlert,
+//         silent: configuration.debug.silent,
+//       }),
+//     ],
+//   });
+// }
