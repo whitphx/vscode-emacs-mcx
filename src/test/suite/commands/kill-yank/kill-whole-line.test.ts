e7ae806ca7d083fbea48abc00bc30d573db5c3e8
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2021-09-26 16:54:21 +0900

    fix/kill yank test with killring (#1039)
    * Update copy-region.test.ts to test with and without killring
    
    * Update tests
    
    * Fix suite title

--- src/test/suite/commands/kill-yank/kill-whole-line.test.ts
+++ src/test/suite/commands/kill-yank/kill-whole-line.test.ts
@@ -2,74 +2,79 @@ import assert from "assert";
 import * as vscode from "vscode";
 import { Position, Selection } from "vscode";
 import { EmacsEmulator } from "../../../../emulator";
+import { KillRing } from "../../../../kill-yank/kill-ring";
 import { assertTextEqual, cleanUpWorkspace, clearTextEditor, setupWorkspace } from "../../utils";
 
-suite("Emulator.killWholeLine", () => {
-  let activeTextEditor: vscode.TextEditor;
+[true, false].forEach((withKillRing) => {
+  suite(`Emulator.killWholeLine, ${withKillRing ? "with" : "without"} killRing`, () => {
+    let activeTextEditor: vscode.TextEditor;
 
-  suite("with non-empty initial text", () => {
-    setup(async () => {
-      const initialText = `0123456789
+    suite("with non-empty initial text", () => {
+      setup(async () => {
+        const initialText = `0123456789
 abcdefghij
 ABCDEFGHIJ`;
-      activeTextEditor = await setupWorkspace(initialText);
-    });
+        activeTextEditor = await setupWorkspace(initialText);
+      });
 
-    teardown(cleanUpWorkspace);
+      teardown(cleanUpWorkspace);
 
-    suite("single cursor in the middle line of the document", () => {
-      const cursorLineNum = 1;
-      const cursorCharNums = [0, 5, 10]; // beginning, middle, end
-      cursorCharNums.forEach((cursorCharNum) => {
-        test(`it works with single cursor at (${cursorLineNum}, ${cursorCharNum})`, async () => {
-          const emulator = new EmacsEmulator(activeTextEditor);
+      suite("single cursor in the middle line of the document", () => {
+        const cursorLineNum = 1;
+        const cursorCharNums = [0, 5, 10]; // beginning, middle, end
+        cursorCharNums.forEach((cursorCharNum) => {
+          test(`it works with single cursor at (${cursorLineNum}, ${cursorCharNum})`, async () => {
+            const emulator = withKillRing
+              ? new EmacsEmulator(activeTextEditor, new KillRing())
+              : new EmacsEmulator(activeTextEditor);
 
-          activeTextEditor.selections = [
-            new Selection(new Position(cursorLineNum, cursorCharNum), new Position(cursorLineNum, cursorCharNum)),
-          ];
+            activeTextEditor.selections = [
+              new Selection(new Position(cursorLineNum, cursorCharNum), new Position(cursorLineNum, cursorCharNum)),
+            ];
 
-          await emulator.runCommand("killWholeLine");
+            await emulator.runCommand("killWholeLine");
 
-          assertTextEqual(
-            activeTextEditor,
-            `0123456789
+            assertTextEqual(
+              activeTextEditor,
+              `0123456789
 ABCDEFGHIJ`
-          );
+            );
 
-          // Check the cut text
-          await clearTextEditor(activeTextEditor);
-          activeTextEditor.selections = [new Selection(new Position(0, 0), new Position(0, 0))];
-          await emulator.runCommand("yank");
-          assert.strictEqual(activeTextEditor.document.getText(), "abcdefghij\n");
+            // Check the cut text
+            await clearTextEditor(activeTextEditor);
+            activeTextEditor.selections = [new Selection(new Position(0, 0), new Position(0, 0))];
+            await emulator.runCommand("yank");
+            assert.strictEqual(activeTextEditor.document.getText(), "abcdefghij\n");
+          });
         });
       });
-    });
 
-    suite("single cursor in the last line of the document", () => {
-      const cursorLineNum = 2; // the last line
-      const cursorCharNums = [0, 5, 10]; // beginning, middle, end
-      cursorCharNums.forEach((cursorCharNum) => {
-        test(`it works with single cursor at (${cursorLineNum}, ${cursorCharNum})`, async () => {
-          const emulator = new EmacsEmulator(activeTextEditor);
+      suite("single cursor in the last line of the document", () => {
+        const cursorLineNum = 2; // the last line
+        const cursorCharNums = [0, 5, 10]; // beginning, middle, end
+        cursorCharNums.forEach((cursorCharNum) => {
+          test(`it works with single cursor at (${cursorLineNum}, ${cursorCharNum})`, async () => {
+            const emulator = new EmacsEmulator(activeTextEditor);
 
-          activeTextEditor.selections = [
-            new Selection(new Position(cursorLineNum, cursorCharNum), new Position(cursorLineNum, cursorCharNum)),
-          ];
+            activeTextEditor.selections = [
+              new Selection(new Position(cursorLineNum, cursorCharNum), new Position(cursorLineNum, cursorCharNum)),
+            ];
 
-          await emulator.runCommand("killWholeLine");
+            await emulator.runCommand("killWholeLine");
 
-          assertTextEqual(
-            activeTextEditor,
-            `0123456789
+            assertTextEqual(
+              activeTextEditor,
+              `0123456789
 abcdefghij
 `
-          );
+            );
 
-          // Check the cut text
-          await clearTextEditor(activeTextEditor);
-          activeTextEditor.selections = [new Selection(new Position(0, 0), new Position(0, 0))];
-          await emulator.runCommand("yank");
-          assert.strictEqual(activeTextEditor.document.getText(), "ABCDEFGHIJ");
+            // Check the cut text
+            await clearTextEditor(activeTextEditor);
+            activeTextEditor.selections = [new Selection(new Position(0, 0), new Position(0, 0))];
+            await emulator.runCommand("yank");
+            assert.strictEqual(activeTextEditor.document.getText(), "ABCDEFGHIJ");
+          });
         });
       });
     });
