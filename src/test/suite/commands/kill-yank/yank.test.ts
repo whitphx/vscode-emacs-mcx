9a823d3a6d37b13f78fe83c471b1049018fbb856
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2021-09-26 16:06:22 +0900

    Update tests

--- src/test/suite/commands/kill-yank/yank.test.ts
+++ src/test/suite/commands/kill-yank/yank.test.ts
@@ -1,168 +1,159 @@
 import * as vscode from "vscode";
 import { Position, Selection } from "vscode";
 import { EmacsEmulator } from "../../../../emulator";
+import { KillRing } from "../../../../kill-yank/kill-ring";
 import { assertCursorsEqual, assertTextEqual, cleanUpWorkspace, setEmptyCursors, setupWorkspace } from "../../utils";
 
-suite("Yank from clipboard, without kill-ring", () => {
-  let activeTextEditor: vscode.TextEditor;
+[true, false].forEach((withKillRing) => {
+  suite(`Yank from clipboard, without kill-ring, ${withKillRing ? "with" : "without"} killRing`, () => {
+    let activeTextEditor: vscode.TextEditor;
+    let emulator: EmacsEmulator;
 
-  suite("with empty initial text", () => {
-    setup(async () => {
-      const initialText = "\n\n"; // 3 lines
-      activeTextEditor = await setupWorkspace(initialText);
-    });
-
-    teardown(cleanUpWorkspace);
-
-    suite("with a single line text in clipboard", () => {
+    suite("with empty initial text", () => {
       setup(async () => {
-        await vscode.env.clipboard.writeText("Lorem ipsum");
+        const initialText = "\n\n"; // 3 lines
+        activeTextEditor = await setupWorkspace(initialText);
+        emulator = withKillRing
+          ? new EmacsEmulator(activeTextEditor, new KillRing())
+          : new EmacsEmulator(activeTextEditor);
       });
 
-      test("it works with single cursor", async () => {
-        const emulator = new EmacsEmulator(activeTextEditor);
+      teardown(cleanUpWorkspace);
 
-        setEmptyCursors(activeTextEditor, [1, 0]);
+      suite("with a single line text in clipboard", () => {
+        setup(async () => {
+          await vscode.env.clipboard.writeText("Lorem ipsum");
+        });
 
-        await emulator.runCommand("yank");
+        test("it works with single cursor", async () => {
+          setEmptyCursors(activeTextEditor, [1, 0]);
 
-        assertTextEqual(activeTextEditor, "\nLorem ipsum\n");
+          await emulator.runCommand("yank");
+
+          assertTextEqual(activeTextEditor, "\nLorem ipsum\n");
+        });
+
+        test("it works with multi cursor", async () => {
+          setEmptyCursors(activeTextEditor, [1, 0], [0, 0], [2, 0]);
+
+          await emulator.runCommand("yank");
+
+          assertTextEqual(activeTextEditor, "Lorem ipsum\nLorem ipsum\nLorem ipsum");
+        });
+
+        test("marks are set when yank", async () => {
+          setEmptyCursors(activeTextEditor, [0, 0]);
+          await emulator.runCommand("yank");
+
+          setEmptyCursors(activeTextEditor, [1, 0]);
+          await emulator.runCommand("yank");
+
+          assertCursorsEqual(activeTextEditor, [1, 11]);
+
+          emulator.popMark();
+          assertCursorsEqual(activeTextEditor, [1, 0]);
+
+          emulator.popMark();
+          assertCursorsEqual(activeTextEditor, [0, 0]);
+        });
       });
 
-      test("it works with multi cursor", async () => {
-        const emulator = new EmacsEmulator(activeTextEditor);
+      suite("with a multiple lines text in clipboard", () => {
+        setup(async () => {
+          await vscode.env.clipboard.writeText("Lorem ipsum\ndolor sit amet,");
+        });
 
-        setEmptyCursors(activeTextEditor, [1, 0], [0, 0], [2, 0]);
+        test("it works with single cursor", async () => {
+          setEmptyCursors(activeTextEditor, [1, 0]);
 
-        await emulator.runCommand("yank");
+          await emulator.runCommand("yank");
 
-        assertTextEqual(activeTextEditor, "Lorem ipsum\nLorem ipsum\nLorem ipsum");
-      });
-
-      test("marks are set when yank", async () => {
-        const emulator = new EmacsEmulator(activeTextEditor);
-
-        setEmptyCursors(activeTextEditor, [0, 0]);
-        await emulator.runCommand("yank");
-
-        setEmptyCursors(activeTextEditor, [1, 0]);
-        await emulator.runCommand("yank");
-
-        assertCursorsEqual(activeTextEditor, [1, 11]);
-
-        emulator.popMark();
-        assertCursorsEqual(activeTextEditor, [1, 0]);
-
-        emulator.popMark();
-        assertCursorsEqual(activeTextEditor, [0, 0]);
-      });
-    });
-
-    suite("with a multiple lines text in clipboard", () => {
-      setup(async () => {
-        await vscode.env.clipboard.writeText("Lorem ipsum\ndolor sit amet,");
-      });
-
-      test("it works with single cursor", async () => {
-        const emulator = new EmacsEmulator(activeTextEditor);
-
-        setEmptyCursors(activeTextEditor, [1, 0]);
-
-        await emulator.runCommand("yank");
-
-        assertTextEqual(
-          activeTextEditor,
-          `
+          assertTextEqual(
+            activeTextEditor,
+            `
 Lorem ipsum
 dolor sit amet,
 `
-        );
-      });
+          );
+        });
 
-      test("it works with multi cursor", async () => {
-        const emulator = new EmacsEmulator(activeTextEditor);
+        test("it works with multi cursor", async () => {
+          setEmptyCursors(activeTextEditor, [1, 0], [0, 0], [2, 0]);
 
-        setEmptyCursors(activeTextEditor, [1, 0], [0, 0], [2, 0]);
+          await emulator.runCommand("yank");
 
-        await emulator.runCommand("yank");
-
-        assertTextEqual(
-          activeTextEditor,
-          `Lorem ipsum
+          assertTextEqual(
+            activeTextEditor,
+            `Lorem ipsum
 dolor sit amet,
 Lorem ipsum
 dolor sit amet,
 Lorem ipsum
 dolor sit amet,`
-        );
-      });
+          );
+        });
 
-      test("it works with same number multi cursor", async () => {
-        const emulator = new EmacsEmulator(activeTextEditor);
+        test("it works with same number multi cursor", async () => {
+          // 2 selections: the same line number to the text on clipboard
+          setEmptyCursors(activeTextEditor, [1, 0], [0, 0]);
 
-        // 2 selections: the same line number to the text on clipboard
-        setEmptyCursors(activeTextEditor, [1, 0], [0, 0]);
+          await emulator.runCommand("yank");
 
-        await emulator.runCommand("yank");
-
-        // Just single paste
-        assertTextEqual(
-          activeTextEditor,
-          `Lorem ipsum
+          // Just single paste
+          assertTextEqual(
+            activeTextEditor,
+            `Lorem ipsum
 dolor sit amet,
 `
-        );
+          );
+        });
       });
     });
-  });
 
-  suite("with non-empty initial text", () => {
-    setup(async () => {
-      const initialText = `0123456789
+    suite("with non-empty initial text", () => {
+      setup(async () => {
+        const initialText = `0123456789
 abcdefghij
 ABCDEFGHIJ`;
-      activeTextEditor = await setupWorkspace(initialText);
-    });
-
-    teardown(cleanUpWorkspace);
-
-    suite("with a single line text in clipboard", () => {
-      setup(async () => {
-        await vscode.env.clipboard.writeText("Lorem ipsum");
+        activeTextEditor = await setupWorkspace(initialText);
       });
 
-      test("it works with single non-empty cursor (selected text are removed)", async () => {
-        const emulator = new EmacsEmulator(activeTextEditor);
+      teardown(cleanUpWorkspace);
 
-        activeTextEditor.selections = [new Selection(new Position(1, 0), new Position(1, 4))];
+      suite("with a single line text in clipboard", () => {
+        setup(async () => {
+          await vscode.env.clipboard.writeText("Lorem ipsum");
+        });
 
-        await emulator.runCommand("yank");
+        test("it works with single non-empty cursor (selected text are removed)", async () => {
+          activeTextEditor.selections = [new Selection(new Position(1, 0), new Position(1, 4))];
 
-        assertTextEqual(
-          activeTextEditor,
-          `0123456789
+          await emulator.runCommand("yank");
+
+          assertTextEqual(
+            activeTextEditor,
+            `0123456789
 Lorem ipsumefghij
 ABCDEFGHIJ`
-        );
-      });
+          );
+        });
 
-      test("it works with multiple non-empty cursor (selected text are removed)", async () => {
-        const emulator = new EmacsEmulator(activeTextEditor);
+        test("it works with multiple non-empty cursor (selected text are removed)", async () => {
+          activeTextEditor.selections = [
+            new Selection(new Position(1, 0), new Position(1, 3)),
+            new Selection(new Position(0, 3), new Position(0, 6)),
+            new Selection(new Position(2, 6), new Position(2, 9)),
+          ];
 
-        activeTextEditor.selections = [
-          new Selection(new Position(1, 0), new Position(1, 3)),
-          new Selection(new Position(0, 3), new Position(0, 6)),
-          new Selection(new Position(2, 6), new Position(2, 9)),
-        ];
+          await emulator.runCommand("yank");
 
-        await emulator.runCommand("yank");
-
-        assertTextEqual(
-          activeTextEditor,
-          `012Lorem ipsum6789
+          assertTextEqual(
+            activeTextEditor,
+            `012Lorem ipsum6789
 Lorem ipsumdefghij
 ABCDEFLorem ipsumJ`
-        );
+          );
+        });
       });
     });
   });
