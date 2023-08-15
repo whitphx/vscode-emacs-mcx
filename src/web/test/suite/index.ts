e7b9c1a4257b128679fae03200ad6463b4f3b5ad
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2023-08-15 11:01:11 +0900

    Set timeout for web tests

--- src/web/test/suite/index.ts
+++ src/web/test/suite/index.ts
@@ -6,6 +6,7 @@ export function run(): Promise<void> {
     mocha.setup({
       ui: "tdd",
       reporter: undefined,
+      timeout: 10000,
     });
 
     // bundles all files in the current directory matching `*.test`
