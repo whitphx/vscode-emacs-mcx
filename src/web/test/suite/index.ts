8b17c3f91ab85b5b4b4af1565ad240665b0776fa
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2023-09-02 00:30:29 +0800

    Revert "Workaround the web tests"
    This reverts commit 0d1c07c3868953395b727c374fbe4299298f92e6.

--- src/web/test/suite/index.ts
+++ src/web/test/suite/index.ts
@@ -1,12 +1,6 @@
 // imports mocha for the browser, defining the `mocha` global.
 require("mocha/mocha");
 
-declare global {
-  var IS_WEB_TEST: boolean | undefined; // eslint-disable-line no-var
-}
-
-globalThis.IS_WEB_TEST = true;
-
 export function run(): Promise<void> {
   return new Promise((c, e) => {
     mocha.setup({
