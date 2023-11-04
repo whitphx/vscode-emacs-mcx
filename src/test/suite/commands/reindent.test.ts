50b1db6fe19c33304aba1623058effe7d0c9b05c
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2023-11-05 00:53:34 +0900

    Add tests

--- src/test/suite/commands/reindent.test.ts
+++ src/test/suite/commands/reindent.test.ts
@@ -75,4 +75,42 @@ suite("Reindent", () => {
       assertCursorsEqual(activeTextEditor, [1, 4]);
     });
   });
+
+  suite("reindent already correctly indent lines", () => {
+    const initialText = `function() {
+  console.log("hello");
+}`;
+
+    setup(async () => {
+      activeTextEditor = await setupWorkspace(initialText, { language: "javascript" });
+      activeTextEditor.options.tabSize = 2;
+      emulator = new EmacsEmulator(activeTextEditor);
+    });
+
+    teardown(cleanUpWorkspace);
+
+    test("reindent does nothing and the cursor is moved to the indent head when the cursor was at the beginning fo the line", async () => {
+      setEmptyCursors(activeTextEditor, [1, 0]);
+      await emulator.runCommand("reindent");
+      assertTextEqual(
+        activeTextEditor,
+        `function() {
+  console.log("hello");
+}`,
+      );
+      assertCursorsEqual(activeTextEditor, [1, 2]);
+    });
+
+    test("reindent does nothing and the cursor is not moved when the cursor was after the indent head", async () => {
+      setEmptyCursors(activeTextEditor, [1, 4]);
+      await emulator.runCommand("reindent");
+      assertTextEqual(
+        activeTextEditor,
+        `function() {
+  console.log("hello");
+}`,
+      );
+      assertCursorsEqual(activeTextEditor, [1, 4]);
+    });
+  });
 });
