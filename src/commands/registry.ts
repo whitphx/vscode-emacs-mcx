6d5d61b758b2277bdba0933ebbe6027d23440f42
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2023-11-05 23:03:39 +0900

    Fix onInterrupt impl

--- src/commands/registry.ts
+++ src/commands/registry.ts
@@ -1,24 +1,28 @@
-import { EmacsCommand } from ".";
+import { EmacsCommand, ITextEditorInterruptionHandler, isTextEditorInterruptionHandler } from ".";
 
 export class EmacsCommandRegistry {
   private commands: Map<string, EmacsCommand>;
+  private interruptionHandlers: ITextEditorInterruptionHandler[];
 
   constructor() {
     this.commands = new Map();
+    this.interruptionHandlers = [];
   }
 
   public register(command: EmacsCommand): void {
     this.commands.set(command.id, command);
+    if (isTextEditorInterruptionHandler(command)) {
+      this.interruptionHandlers.push(command);
+    }
   }
 
   public get(commandName: string): EmacsCommand | undefined {
     return this.commands.get(commandName);
   }
 
-  public forEach(
-    callbackfn: (value: EmacsCommand, key: string, map: Map<string, EmacsCommand>) => void,
-    thisArg?: unknown,
-  ): void {
-    this.commands.forEach(callbackfn, thisArg);
+  public onInterrupt(): void {
+    for (const handler of this.interruptionHandlers) {
+      handler.onDidInterruptTextEditor();
+    }
   }
 }
