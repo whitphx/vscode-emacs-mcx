560b14a0d31596e8532f81db26712c00efdf32ad
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2023-11-05 16:56:41 +0900

    Remove workarounds from src/test/suite/commands/new-line.test.ts

--- src/test/suite/commands/new-line.test.ts
+++ src/test/suite/commands/new-line.test.ts
@@ -1,7 +1,7 @@
 import assert from "assert";
 import * as vscode from "vscode";
 import { EmacsEmulator } from "../../../emulator";
-import { assertTextEqual, cleanUpWorkspace, clearTextEditor, setEmptyCursors, setupWorkspace } from "../utils";
+import { assertTextEqual, cleanUpWorkspace, setEmptyCursors, setupWorkspace } from "../utils";
 
 const eols: Array<[vscode.EndOfLine, string]> = [
   [vscode.EndOfLine.LF, "\n"],
@@ -118,32 +118,10 @@ eols.forEach(([eol, eolStr]) => {
         assert.strictEqual(activeTextEditor.selection.active.character, 4);
       });
 
-      const languagesAutoDoc = [
-        // "c", "cpp"  // Auto-indent for doc comments does not work with these languages in test env while I don't know why...
-        "javascript",
-        "javascriptreact",
-        "typescript",
-        "typescriptreact",
-      ];
+      const languagesAutoDoc = ["c", "cpp", "javascript", "javascriptreact", "typescript", "typescriptreact"];
       languagesAutoDoc.forEach((language) => {
         test(`newLine does not disable the language specific control in case of ${language}`, async function () {
           const initialText = "/** */";
-
-          // XXX: First, trigger the language's auto-indent feature without any assertion before the main test execution.
-          // This is necessary for the test to be successful at VSCode 1.50.
-          // It may be because the first execution warms up the language server.
-          // TODO: Remove this workaround with later versions of VSCode
-          activeTextEditor = await setupWorkspace(initialText, {
-            eol,
-            language,
-          });
-          emulator = new EmacsEmulator(activeTextEditor);
-
-          setEmptyCursors(activeTextEditor, [0, 3]);
-
-          await vscode.commands.executeCommand("default:type", { text: "\n" });
-          await clearTextEditor(activeTextEditor);
-          // XXX: (end of the workaround)
 
           activeTextEditor = await setupWorkspace(initialText, {
             eol,
@@ -227,32 +205,10 @@ eols.forEach(([eol, eolStr]) => {
         assertTextEqual(activeTextEditor, `(${eolStr}${eolStr}${eolStr}${eolStr}    ${eolStr})`);
       });
 
-      const languagesAutoDoc = [
-        // "c", "cpp"  // Auto-indent for doc comments does not work with these languages in test env while I don't know why...
-        "javascript",
-        "javascriptreact",
-        "typescript",
-        "typescriptreact",
-      ];
+      const languagesAutoDoc = ["c", "cpp", "javascript", "javascriptreact", "typescript", "typescriptreact"];
       languagesAutoDoc.forEach((language) => {
         test(`newLine does not disable the language specific control in case of ${language}`, async function () {
           const initialText = "/** */";
-
-          // XXX: First, trigger the language's auto-indent feature without any assertion before the main test execution.
-          // This is necessary for the test to be successful at VSCode 1.50.
-          // It may be because the first execution warms up the language server.
-          // TODO: Remove this workaround with later versions of VSCode
-          activeTextEditor = await setupWorkspace(initialText, {
-            eol,
-            language,
-          });
-          emulator = new EmacsEmulator(activeTextEditor);
-
-          setEmptyCursors(activeTextEditor, [0, 3]);
-
-          await vscode.commands.executeCommand("default:type", { text: "\n" });
-          await clearTextEditor(activeTextEditor);
-          // XXX: (end of the workaround)
 
           activeTextEditor = await setupWorkspace(initialText, {
             eol,
