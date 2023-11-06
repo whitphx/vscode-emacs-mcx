a83cf1db46dc92bf6fee9333bc263d7c6b58c70e
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2023-11-07 01:21:51 +0900

    Add a test for `isearchExit` with mark mode

--- src/test/suite/commands/find.test.ts
+++ src/test/suite/commands/find.test.ts
@@ -1,4 +1,5 @@
 import * as vscode from "vscode";
+import * as assert from "assert";
 import { EmacsEmulator } from "../../../emulator";
 import { assertCursorsEqual, setEmptyCursors, setupWorkspace, cleanUpWorkspace, assertSelectionsEqual } from "../utils";
 
@@ -38,9 +39,23 @@ suite("isearch", () => {
     // This is different from the original Emacs behavior which doesn't select the matched text after exiting isearch.
     // But I think this is more useful respecting the VSCode's behavior.
     assertSelectionsEqual(activeTextEditor, [2, 0, 2, 3]);
+    assert.strictEqual(emulator.isInMarkMode, false);
 
     emulator.popMark();
 
     assertCursorsEqual(activeTextEditor, [1, 1]);
+    assert.strictEqual(emulator.isInMarkMode, false);
+  });
+
+  test("isearchExit keeps the mark mode", async () => {
+    setEmptyCursors(activeTextEditor, [1, 1]);
+
+    await emulator.setMarkCommand();
+    await emulator.runCommand("isearchForward", [{ searchString: "aaa" }]);
+
+    await emulator.runCommand("isearchExit");
+
+    assertSelectionsEqual(activeTextEditor, [1, 1, 2, 3]);
+    assert.strictEqual(emulator.isInMarkMode, true);
   });
 });
