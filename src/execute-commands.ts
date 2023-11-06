9722839d7b494271ff11566206f8c0872f39ab1d
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2023-11-06 12:29:38 +0900

    Use the builtin `runCommands` command instead of `emacs-mcx.executeCommands`

--- src/execute-commands.ts
+++
@@ -1,5 +0,0 @@
-import * as vscode from "vscode";
-
-export async function executeCommands(commands: string[]): Promise<void> {
-  await Promise.all(commands.map((command) => vscode.commands.executeCommand(command)));
-}
--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,6 @@ import { Configuration } from "./configuration/configuration";
 import { WorkspaceConfigCache } from "./workspace-configuration";
 import { EmacsEmulator } from "./emulator";
 import { EmacsEmulatorMap } from "./emulator-map";
-import { executeCommands } from "./execute-commands";
 import { KillRing } from "./kill-yank/kill-ring";
 import { logger } from "./logger";
 import { MessageManager } from "./message";
@@ -395,12 +394,6 @@ export function activate(context: vscode.ExtensionContext): void {
       return;
     }
     return emulator.insertRegister(arg);
-  });
-
-  vscode.commands.registerCommand("emacs-mcx.executeCommands", async (...args: any[]) => {
-    if (1 <= args.length) {
-      executeCommands(args[0]);
-    }
   });
 
   registerEmulatorCommand("emacs-mcx.executeCommandWithPrefixArgument", (emulator, args) => {
