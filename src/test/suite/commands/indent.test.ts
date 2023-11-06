a4f47e4493eab374de29570be83a30ee09ef9a56
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2023-11-06 13:27:59 +0900

    Fix

--- src/test/suite/commands/indent.test.ts
+++ src/test/suite/commands/indent.test.ts
@@ -6,22 +6,20 @@ suite("DeleteIndentation", () => {
   let activeTextEditor: vscode.TextEditor;
   let emulator: EmacsEmulator;
 
-  suite("merge 2 lines", () => {
-    const initialText = "0123456789\nabcdefghij\nABCDEFGHIJ";
+  const initialText = "0123456789\nabcdefghij\nABCDEFGHIJ";
 
-    setup(async () => {
-      activeTextEditor = await setupWorkspace(initialText, { language: "javascript" });
-      activeTextEditor.options.tabSize = 2;
-      emulator = new EmacsEmulator(activeTextEditor);
-    });
+  setup(async () => {
+    activeTextEditor = await setupWorkspace(initialText, { language: "javascript" });
+    activeTextEditor.options.tabSize = 2;
+    emulator = new EmacsEmulator(activeTextEditor);
+  });
 
-    teardown(cleanUpWorkspace);
+  teardown(cleanUpWorkspace);
 
-    test("merge the current line with the previous line", async () => {
-      setEmptyCursors(activeTextEditor, [1, 2]);
-      await emulator.runCommand("deleteIndentation");
-      assertTextEqual(activeTextEditor, "0123456789 abcdefghij\nABCDEFGHIJ");
-      assertCursorsEqual(activeTextEditor, [0, 10]);
-    });
+  test("merge the current line with the previous line", async () => {
+    setEmptyCursors(activeTextEditor, [1, 2]);
+    await emulator.runCommand("deleteIndentation");
+    assertTextEqual(activeTextEditor, "0123456789 abcdefghij\nABCDEFGHIJ");
+    assertCursorsEqual(activeTextEditor, [0, 10]);
   });
 });
