42f90344232cec6c1cf5c11dcd1314566bf0db49
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2024-01-11 19:02:36 +0900

    Fix lint-staged config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -7,10 +7,16 @@ const micromatch = require("micromatch");
 module.exports = {
   "*.{js,ts,mjs,mts}": (files) => {
     const match = micromatch.not(files, path.join(__dirname, "./vendor/*"));
+    if (match.length === 0) {
+      return "";
+    }
     return `eslint --cache --fix ${match.join(" ")}`;
   },
   "*.{js,ts,mjs,mts,md,json,yml}": (files) => {
     const match = micromatch.not(files, path.join(__dirname, "./vendor/*"));
+    if (match.length === 0) {
+      return "";
+    }
     return `prettier --write ${match.join(" ")}`;
   },
 };
