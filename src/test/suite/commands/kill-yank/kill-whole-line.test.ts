307ef212a8886959878c654979d3defe432ce6b6
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2024-01-13 23:44:50 +0900

    Refactoring kill-whole-line.test.ts

--- src/test/suite/commands/kill-yank/kill-whole-line.test.ts
+++ src/test/suite/commands/kill-yank/kill-whole-line.test.ts
@@ -10,92 +10,82 @@ import { assertTextEqual, cleanUpWorkspace, clearTextEditor, setupWorkspace, set
     let activeTextEditor: vscode.TextEditor;
     let emulator: EmacsEmulator;
 
-    suite("with non-empty initial text", () => {
-      setup(async () => {
-        const initialText = `0123456789
+    setup(async () => {
+      const initialText = `0123456789
 abcdefghij
 ABCDEFGHIJ`;
-        activeTextEditor = await setupWorkspace(initialText);
+      activeTextEditor = await setupWorkspace(initialText);
 
-        emulator = withKillRing
-          ? new EmacsEmulator(activeTextEditor, new KillRing())
-          : new EmacsEmulator(activeTextEditor);
-      });
+      emulator = withKillRing
+        ? new EmacsEmulator(activeTextEditor, new KillRing())
+        : new EmacsEmulator(activeTextEditor);
+    });
 
-      teardown(cleanUpWorkspace);
+    teardown(cleanUpWorkspace);
 
-      suite("single cursor in the middle line of the document", () => {
-        const cursorLineNum = 1;
-        const cursorCharNums = [0, 5, 10]; // beginning, middle, end
-        cursorCharNums.forEach((cursorCharNum) => {
-          test(`it works with single cursor at (${cursorLineNum}, ${cursorCharNum})`, async () => {
-            activeTextEditor.selections = [
-              new Selection(new Position(cursorLineNum, cursorCharNum), new Position(cursorLineNum, cursorCharNum)),
-            ];
+    [0, 5, 10].forEach((cursorCharNum) => {
+      // beginning, middle, end
+      test(`single cursor in the middle line of the document, at (1, ${cursorCharNum})`, async () => {
+        const cursorLineNum = 1; // the middle line
+        setEmptyCursors(activeTextEditor, [cursorLineNum, cursorCharNum]);
 
-            await emulator.runCommand("killWholeLine");
+        await emulator.runCommand("killWholeLine");
 
-            assertTextEqual(
-              activeTextEditor,
-              `0123456789
+        assertTextEqual(
+          activeTextEditor,
+          `0123456789
 ABCDEFGHIJ`,
-            );
+        );
 
-            // Check the cut text
-            await clearTextEditor(activeTextEditor);
-            activeTextEditor.selections = [new Selection(new Position(0, 0), new Position(0, 0))];
-            await emulator.runCommand("yank");
-            assert.strictEqual(activeTextEditor.document.getText(), "abcdefghij\n");
-          });
-        });
+        // Check the cut text
+        await clearTextEditor(activeTextEditor);
+        activeTextEditor.selections = [new Selection(new Position(0, 0), new Position(0, 0))];
+        await emulator.runCommand("yank");
+        assert.strictEqual(activeTextEditor.document.getText(), "abcdefghij\n");
       });
+    });
 
-      suite("single cursor in the last line of the document", () => {
+    [0, 5, 10].forEach((cursorCharNum) => {
+      // beginning, middle, end
+      test(`single cursor in the last line of the document, at (2, ${cursorCharNum})`, async () => {
         const cursorLineNum = 2; // the last line
-        const cursorCharNums = [0, 5, 10]; // beginning, middle, end
-        cursorCharNums.forEach((cursorCharNum) => {
-          test(`it works with single cursor at (${cursorLineNum}, ${cursorCharNum})`, async () => {
-            activeTextEditor.selections = [
-              new Selection(new Position(cursorLineNum, cursorCharNum), new Position(cursorLineNum, cursorCharNum)),
-            ];
+        setEmptyCursors(activeTextEditor, [cursorLineNum, cursorCharNum]);
 
-            await emulator.runCommand("killWholeLine");
+        await emulator.runCommand("killWholeLine");
 
-            assertTextEqual(
-              activeTextEditor,
-              `0123456789
+        assertTextEqual(
+          activeTextEditor,
+          `0123456789
 abcdefghij
 `,
-            );
+        );
 
-            // Check the cut text
-            await clearTextEditor(activeTextEditor);
-            activeTextEditor.selections = [new Selection(new Position(0, 0), new Position(0, 0))];
-            await emulator.runCommand("yank");
-            assert.strictEqual(activeTextEditor.document.getText(), "ABCDEFGHIJ");
-          });
-        });
+        // Check the cut text
+        await clearTextEditor(activeTextEditor);
+        activeTextEditor.selections = [new Selection(new Position(0, 0), new Position(0, 0))];
+        await emulator.runCommand("yank");
+        assert.strictEqual(activeTextEditor.document.getText(), "ABCDEFGHIJ");
       });
+    });
 
-      (["mark-mode", "rectangle-mode"] as const).forEach((mode) => {
-        test(`${mode} doesn't affect the behavior`, async () => {
-          setEmptyCursors(activeTextEditor, [0, 0]);
-          if (mode === "mark-mode") {
-            emulator.setMarkCommand();
-          } else if (mode === "rectangle-mode") {
-            emulator.rectangleMarkMode();
-          }
-          await emulator.runCommand("nextLine");
-          await emulator.runCommand("forwardChar");
-          // Now the cursor is at [1, 1]
+    (["mark-mode", "rectangle-mode"] as const).forEach((mode) => {
+      test(`${mode} doesn't affect the behavior`, async () => {
+        setEmptyCursors(activeTextEditor, [0, 0]);
+        if (mode === "mark-mode") {
+          emulator.setMarkCommand();
+        } else if (mode === "rectangle-mode") {
+          emulator.rectangleMarkMode();
+        }
+        await emulator.runCommand("nextLine");
+        await emulator.runCommand("forwardChar");
+        // Now the cursor is at [1, 1]
 
-          await emulator.runCommand("killWholeLine");
-          assertTextEqual(
-            activeTextEditor,
-            `0123456789
+        await emulator.runCommand("killWholeLine");
+        assertTextEqual(
+          activeTextEditor,
+          `0123456789
 ABCDEFGHIJ`,
-          );
-        });
+        );
       });
     });
   });
