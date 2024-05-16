b092cc61c286ba44e4f1025378b4bd825991cab8
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2024-05-16 23:57:38 +0900

    Refactoring

--- src/commands/tab.ts
+++ src/commands/tab.ts
@@ -17,73 +17,7 @@ export class TabToTabStop extends EmacsCommand {
     prefixArgument: number | undefined,
   ): Promise<unknown> {
     if (offsideRuleLanguageIds.includes(textEditor.document.languageId)) {
-      const tabSize = textEditor.options.tabSize as number;
-      const insertSpaces = textEditor.options.insertSpaces as boolean;
-
-      const singleIndentSize = insertSpaces ? tabSize : 1;
-
-      const indentOps = textEditor.selections.map((selection, i) => {
-        let prevNonEmptyLine: vscode.TextLine | undefined;
-        for (let i = selection.active.line - 1; i >= 0; i--) {
-          const line = textEditor.document.lineAt(i);
-          if (line.isEmptyOrWhitespace) {
-            continue;
-          }
-          prevNonEmptyLine = line;
-          break;
-        }
-        const prevLineIndentChars = prevNonEmptyLine?.firstNonWhitespaceCharacterIndex;
-
-        const maxIndentLevel = prevLineIndentChars != null ? Math.floor(prevLineIndentChars / tabSize) + 1 : 0;
-
-        const prevIndentLevel = this.latestIndentLevels[i]; // IndentUnits applied in the previous call
-        const newIndentLevel =
-          prevIndentLevel != null && 0 < prevIndentLevel && prevIndentLevel <= maxIndentLevel
-            ? prevIndentLevel - 1
-            : maxIndentLevel;
-
-        const newIndentChars = newIndentLevel * singleIndentSize;
-
-        const thisLine = textEditor.document.lineAt(selection.active.line);
-        const charsOffsetAfterIndent = Math.max(
-          selection.active.character - thisLine.firstNonWhitespaceCharacterIndex,
-          0,
-        );
-
-        return {
-          line: selection.active.line,
-          newIndentLevel,
-          newIndentChars,
-          newCursorChars: newIndentChars + charsOffsetAfterIndent,
-        };
-      });
-
-      // Update the indents
-      const indentChar = insertSpaces ? " " : "\t";
-      await textEditor.edit((editBuilder) => {
-        indentOps.forEach((indentOp) => {
-          const line = textEditor.document.lineAt(indentOp.line);
-          const lineHead = line.range.start;
-          const charsToInsertOrDelete = indentOp.newIndentChars - line.firstNonWhitespaceCharacterIndex;
-          if (charsToInsertOrDelete >= 0) {
-            editBuilder.insert(lineHead, indentChar.repeat(charsToInsertOrDelete));
-          } else {
-            editBuilder.delete(new vscode.Range(lineHead, new vscode.Position(indentOp.line, -charsToInsertOrDelete)));
-          }
-        });
-      });
-      // Update the cursor positions
-      textEditor.selections = indentOps.map((indentOp) => {
-        const active = new vscode.Position(indentOp.line, indentOp.newCursorChars);
-        return new vscode.Selection(active, active);
-      });
-
-      this.latestIndentLevels = indentOps.map((indentOp) => indentOp.newIndentLevel);
-      this.latestTextEditor = textEditor;
-      this.latestSelections = textEditor.selections;
-
-      this.emacsController.exitMarkMode();
-      return;
+      return this.runInOffsideRuleLanguage(textEditor);
     }
 
     // A single call of `editor.action.reindentselectedlines`
@@ -102,6 +36,75 @@ export class TabToTabStop extends EmacsCommand {
       });
       this.emacsController.exitMarkMode();
     });
+  }
+
+  private async runInOffsideRuleLanguage(textEditor: TextEditor): Promise<void> {
+    const tabSize = textEditor.options.tabSize as number;
+    const insertSpaces = textEditor.options.insertSpaces as boolean;
+
+    const singleIndentSize = insertSpaces ? tabSize : 1;
+
+    const indentOps = textEditor.selections.map((selection, i) => {
+      let prevNonEmptyLine: vscode.TextLine | undefined;
+      for (let i = selection.active.line - 1; i >= 0; i--) {
+        const line = textEditor.document.lineAt(i);
+        if (line.isEmptyOrWhitespace) {
+          continue;
+        }
+        prevNonEmptyLine = line;
+        break;
+      }
+      const prevLineIndentChars = prevNonEmptyLine?.firstNonWhitespaceCharacterIndex;
+
+      const maxIndentLevel = prevLineIndentChars != null ? Math.floor(prevLineIndentChars / tabSize) + 1 : 0;
+
+      const prevIndentLevel = this.latestIndentLevels[i]; // IndentUnits applied in the previous call
+      const newIndentLevel =
+        prevIndentLevel != null && 0 < prevIndentLevel && prevIndentLevel <= maxIndentLevel
+          ? prevIndentLevel - 1
+          : maxIndentLevel;
+
+      const newIndentChars = newIndentLevel * singleIndentSize;
+
+      const thisLine = textEditor.document.lineAt(selection.active.line);
+      const charsOffsetAfterIndent = Math.max(
+        selection.active.character - thisLine.firstNonWhitespaceCharacterIndex,
+        0,
+      );
+
+      return {
+        line: selection.active.line,
+        newIndentLevel,
+        newIndentChars,
+        newCursorChars: newIndentChars + charsOffsetAfterIndent,
+      };
+    });
+
+    // Update the indents
+    const indentChar = insertSpaces ? " " : "\t";
+    await textEditor.edit((editBuilder) => {
+      indentOps.forEach((indentOp) => {
+        const line = textEditor.document.lineAt(indentOp.line);
+        const lineHead = line.range.start;
+        const charsToInsertOrDelete = indentOp.newIndentChars - line.firstNonWhitespaceCharacterIndex;
+        if (charsToInsertOrDelete >= 0) {
+          editBuilder.insert(lineHead, indentChar.repeat(charsToInsertOrDelete));
+        } else {
+          editBuilder.delete(new vscode.Range(lineHead, new vscode.Position(indentOp.line, -charsToInsertOrDelete)));
+        }
+      });
+    });
+    // Update the cursor positions
+    textEditor.selections = indentOps.map((indentOp) => {
+      const active = new vscode.Position(indentOp.line, indentOp.newCursorChars);
+      return new vscode.Selection(active, active);
+    });
+
+    this.latestIndentLevels = indentOps.map((indentOp) => indentOp.newIndentLevel);
+    this.latestTextEditor = textEditor;
+    this.latestSelections = textEditor.selections;
+
+    this.emacsController.exitMarkMode();
   }
 
   public onDidInterruptTextEditor(): void {
