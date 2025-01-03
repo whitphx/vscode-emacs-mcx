7c5b017acfad629d70e03b471f0ea6954963ba68
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2025-01-03 18:57:10 +0900

    Remove unnecessary code

--- src/message.ts
+++ src/message.ts
@@ -20,15 +20,11 @@ export class MessageManager implements vscode.Disposable {
   }
 
   public static showMessage(text: string): void {
-    if (this.instance) {
-      this.instance.showMessage(text);
-    }
+    this.instance.showMessage(text);
   }
 
   public static removeMessage(): void {
-    if (this.instance) {
-      this.instance.removeMessage();
-    }
+    this.instance.removeMessage();
   }
 
   private static inst: MessageManager;
