9182fabf09e2517631a2dda18f5908baf27d810b
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2025-07-07 03:53:14 +0900

    Enable ESLint typed lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,13 @@ import tseslint from "typescript-eslint";
 
 export default tseslint.config({
   ignores: ["src/vs/**"],
-  extends: [eslint.configs.recommended, ...tseslint.configs.recommended],
+  extends: [eslint.configs.recommended, ...tseslint.configs.recommendedTypeChecked],
+  languageOptions: {
+    parserOptions: {
+      projectService: true,
+      tsconfigRootDir: import.meta.dirname,
+    },
+  },
   rules: {
     "@typescript-eslint/no-unused-vars": [
       "error",
