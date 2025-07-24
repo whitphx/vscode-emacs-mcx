d58989d4ee495642b711f0908b14afbba9cb3923
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2025-07-24 18:17:51 +0200

    Fix eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,22 +2,41 @@
 
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
+import path from "node:path";
 
-export default tseslint.config({
-  ignores: ["src/vs/**"],
-  extends: [eslint.configs.recommended, ...tseslint.configs.recommendedTypeChecked],
-  languageOptions: {
-    parserOptions: {
-      projectService: true,
-      tsconfigRootDir: import.meta.dirname,
+const config = tseslint.config(
+  {
+    basePath: "src",
+    ignores: ["vs/**"],
+    extends: [eslint.configs.recommended, ...tseslint.configs.recommendedTypeChecked],
+    // Ref: https://typescript-eslint.io/getting-started/typed-linting/
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "textEditor|isInMarkMode|prefixArgument",
+        },
+      ],
     },
   },
-  rules: {
-    "@typescript-eslint/no-unused-vars": [
-      "error",
-      {
-        argsIgnorePattern: "textEditor|isInMarkMode|prefixArgument",
+  {
+    basePath: "keybinding-generator",
+    ignores: ["run-in-vscode/**/*"],
+    extends: [eslint.configs.recommended, ...tseslint.configs.recommendedTypeChecked],
+    // Ref: https://typescript-eslint.io/getting-started/typed-linting/
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: path.join(import.meta.dirname, "keybinding-generator"),
       },
-    ],
+    },
   },
-});
+);
+
+export default config;
