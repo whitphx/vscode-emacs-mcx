c8d237f7a0553fd1349cbee3e8e8a37f666e1604
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2025-12-09 23:36:37 +0900

    restore comment

--- src/commands/helpers/wordSeparators.ts
+++ src/commands/helpers/wordSeparators.ts
@@ -3,8 +3,10 @@ import { getMapForWordSeparators } from "vs/editor/common/controller/wordCharact
 
 // Shared helper to honor editor.wordSeparators
 export function getWordSeparators() {
+  // Ref: https://github.com/VSCodeVim/Vim/blob/91ca71f8607458c0558f9aff61e230c6917d4b51/src/configuration/configuration.ts#L155
   const activeTextEditor = vscode.window.activeTextEditor;
   const resource = activeTextEditor ? activeTextEditor.document.uri : null;
   const maybeWordSeparators = vscode.workspace.getConfiguration("editor", resource).wordSeparators as unknown;
+  // Ref: https://github.com/microsoft/vscode/blob/bc9f2577cd8e297b003e5ca652e19685504a1e50/src/vs/editor/contrib/wordOperations/wordOperations.ts#L45
   return getMapForWordSeparators(typeof maybeWordSeparators === "string" ? maybeWordSeparators : "");
 }
